787e26005ad86c606b77890703bfc2e3d7648b4f
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2023-06-18 09:34:41 +0200

    made static and added base path

--- 005_overengineerd-voci-learning/src/routes/+layout.svelte
+++ 005_overengineerd-voci-learning/src/routes/+layout.svelte
@@ -1,13 +1,15 @@
 <script>
     import '../app.css';
+
+    import  { base } from '$app/paths';
 </script>
 
 <div class="bg-neutral-300">
-    <a class="p-2 inline-block" href="edit">Edit a Vocabulary Chart</a>
+    <a class="inline-block p-2" href={`${base}/edit`}>Edit a Vocabulary Chart</a>
     or
-    <a class="p-2 inline-block" href="learn">Learn</a>
+    <a class="inline-block p-2" href={`${base}/learn`}>Learn</a>
 
-    | <a class="p-2 inline-block" href="/">Home</a>
+    | <a class="inline-block p-2" href={`${base}/`}>Home</a>
 </div>
 
 <slot></slot>
