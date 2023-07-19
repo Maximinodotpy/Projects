5794c35a0b06cac15945b35148ded6b4b31a2dcf
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2023-07-19 15:36:46 +0200

    Update svelte.config.js

--- 005_voco-loco/svelte.config.js
+++ 005_voco-loco/svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 	kit: {
 
 		paths: {
-			base: '/d/loco-voco',
+			base: '/d/voco-loco',
 		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
