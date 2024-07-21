4dbe0a32d491373281110b90c2232db2ebac72f7
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2024-07-21 18:26:02 +0200

    G

--- 022_politik_landschaft_von_schaffhausen/svelte.config.js
+++ 022_politik_landschaft_von_schaffhausen/svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,11 +8,13 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
+	
+
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'dist',
+			assets: 'dist',
+		})
 	}
 };
 
