462232d781865b8a78dad5491782109a0d460a38
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2024-07-22 21:28:00 +0200

    Update +page.svelte

--- 022_politik_landschaft_von_schaffhausen/src/routes/partei/+page.svelte
+++ 022_politik_landschaft_von_schaffhausen/src/routes/partei/+page.svelte
@@ -5,31 +5,37 @@
     import { allPeopleGroupedByParty, partiesMatchedWithWhereTheyAreRepresentedLegaslitativaly, partiesMatchedWithWhereTheyAreRepresentedExekutivaly } from "$lib/index";
 
     import PeopleTable from "$lib/PeopleTable.svelte";
-    import CommunityList from "$lib/communityList.svelte";
+    import CommunityList from "$lib/CommunityList.svelte";
     import PageTitle from "$lib/PageTitle.svelte";
 
     let partyName = $page.url.searchParams.get("p") as string;
-    let thisParty = $allPeopleGroupedByParty[partyName];    
+
+    let thisParty = $allPeopleGroupedByParty[partyName];
 </script>
 
 <PageTitle title={partyName} category="Partei" />
 
-<div class="mb-20">
-    <h2 class="tracking-widest text-2xl mb-4">Alle Personen in dieser Partei ({thisParty.length})</h2>
-
-    <PeopleTable data={thisParty} hideParty={true} />
-</div>
-
-<div class="grid grid-cols-2 gap-4">
+{#if !(partyName in $allPeopleGroupedByParty) || thisParty.length == 0}
+    <p>Daten werden geladen ...</p>
+{:else}
     <div class="mb-20">
-        <h2 class="tracking-widest text-2xl mb-4">Vertreten in Folgenden Exekutiven</h2>
-    
-        <CommunityList communities={$partiesMatchedWithWhereTheyAreRepresentedExekutivaly[partyName]} />
+        <h2 class="tracking-widest text-2xl mb-4">Alle Personen in dieser Partei ({thisParty.length})</h2>
+
+        <PeopleTable data={thisParty} hideParty={true} />
     </div>
-    
-    <div class="mb-20">
-        <h2 class="tracking-widest text-2xl mb-4">Vertreten in Folgenden Legislativen</h2>
-    
-        <CommunityList communities={$partiesMatchedWithWhereTheyAreRepresentedLegaslitativaly[partyName]} />
+
+    <div class="grid grid-cols-2 gap-4">
+        <div class="mb-20">
+            <h2 class="tracking-widest text-2xl mb-4">Vertreten in Folgenden Exekutiven</h2>
+        
+            <CommunityList communities={$partiesMatchedWithWhereTheyAreRepresentedExekutivaly[partyName]} />
+        </div>
+        
+        <div class="mb-20">
+            <h2 class="tracking-widest text-2xl mb-4">Vertreten in Folgenden Legislativen</h2>
+        
+            <CommunityList communities={$partiesMatchedWithWhereTheyAreRepresentedLegaslitativaly[partyName]} />
+        </div>
     </div>
-</div>
+{/if}
+
