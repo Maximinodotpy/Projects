6540acd1c1c6fc9e84aafa8e21a724c1a993b8e0
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2024-08-03 14:33:49 +0200

    rm plvs

--- 022_politik_landschaft_von_schaffhausen/src/lib/PartyList.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { partieslistData } from "$lib";
-</script>
-
-<div class="flex flex-col items-start">
-    {#each $partieslistData as party}
-        <a href="partei?p={party}">{party}</a>
-    {/each}
-</div>
